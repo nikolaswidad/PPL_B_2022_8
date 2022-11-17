ac0d2fa4b6b6e56bfe625521f0b8e2bfffd7501a
SabdielTarigan <112071041+SabdielTarigan@users.noreply.github.com>
2022-11-17 15:10:17 +0700

    Update mhs_pkl.php

--- mhs_pkl.php
+++ mhs_pkl.php
@@ -157,32 +157,29 @@ $nim = $_SESSION['nim'];
             </div>
         </div>
     </div>
-//a
-   //a
-    //a
-    //a
-    //a
-    //a
-    //a
-    //a
-    //a
-    //a
-    //a
-    //a
-    //a
-    //a
-   //a
-    //a
-    //a
-    //a
-    //a
-    //a
-    //a
-    //a
-    //a
-    //a
-    //a
-    //a
+<!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript--><!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript--><!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript-->
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
