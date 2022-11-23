0778ac3173b35897fd58022dd433d2888ba7ed32
nikolaswidad <70227834+nikolaswidad@users.noreply.github.com>
2022-11-23 23:05:09 +0700

    FE lanjutan naufal verif

--- dosen_verif_irs.php
+++ dosen_verif_irs.php
@@ -9,110 +9,93 @@ $nim = $_SESSION['username'];
 
 <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SIAP-SIAP</title>
-    <?php include('href.html')?>
-
+    <?php include('href.html');?>
+    
 </head>
 
 <body id="page-top">
-
+    
     <!-- Page Wrapper -->
     <div id="wrapper">
-
-        <!-- Sidebar -->
-        <!-- Sidebar -->
-    <?php include('dosen_nav.html')?>
-        <!-- End of Sidebar -->
-
+        
+        <?php include('dosen_nav.html');?>
+        
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
-
+            
             <!-- Main Content -->
             <div id="content">
-            <?php include('header.html')?>
-            
-            <div class="card">
-                    <!-- If there is success variable, show message -->
-                    
-                    <div class="card-header">Data Mahasiswa</div>
-                    <div class="form-group row mt-2">
-                    <div class="col-sm-10">
-                      <input type="text" name="keyword" class="form-control" id="">
-                    </div>
-                    <input type="submit" value="Cari">
-                  </div>
+                <?php include('header.html');?>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
                 
-            <br>
-                    <div class="card-body">
-                    <table class="table table-striped table-hover">
-                        <thead>
-                            <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">Nama</th>
-                            <th scope="col">NIM</th>
-                            <th scope="col">Angkatan</th>
-                            <th scope="col">Verif</th>
-                            <th scope="col">Detail</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        <?php
-                        
-                        
 
-                        
+                    <!-- Page Heading -->
+                    <!-- <h1 class="h3 mb-2 text-gray-800">Tables</h1>
+                    <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
+                        For more information about DataTables, please visit the <a target="_blank"
+                            href="https://datatables.net">official DataTables documentation</a>.</p> -->
 
-                        
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Verifikasi Data IRS</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">No</th>
+                                            <th scope="col">Nama</th>
+                                            <th scope="col">NIM</th>
+                                            <th scope="col">Angkatan</th>
+                                            <th scope="col">Verif</th>
+                                            <th scope="col">Scan</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php
+                                        if(isset($_GET['keyword'])){
+                                            $cari = $_GET['cari_jenis_barang'];
+                                            $data = mysqli_query($conn, "SELECT * FROM mhs INNER JOIN irs ON irs.nim = mhs.nim WHERE mhs.nama LIKE '%".$Cari."%'");
+                                        }else{
+                                            $data = mysqli_query($conn, "SELECT * FROM mhs INNER JOIN irs ON irs.nim = mhs.nim");
+                                        }
+                                        $no = 1;
+                                        while ($row = mysqli_fetch_array($data)) {
+                                            echo "<tr>";
+                                            echo "<th>".$no."</th>";
+                                            echo "<td>".$row['nama']."</td>";
+                                            echo "<td>".$row['nim']."</td>";
+                                            echo "<td>".$row['angkatan']."</td>";
+                                            echo "<td>".$row['verif']."</td>";
+                                            echo '<td><a class="btn btn-info" href="upload/' .$row['scan'] . '">Lihat</a></td>';
+                                            echo '<td><a class="btn btn-warning" href="verif_irs.php?nim='.$row['nim'].'">Verif</a>  
+                                            <a class="btn btn-danger" href="cancel_verif_irs.php?nim='.$row["nim"].'">Cancel</a> </td> ';
+                                            echo "</tr>";
+                                            $no++;
+                                            
+                                        }
+                                        echo '</tbody>';
+                                        echo '</table>';
                     
+                                        $data->free();
+                                        $conn->close();
 
-                        if(isset($_GET['keyword'])){
-                            $cari = $_GET['cari_jenis_barang'];
-                            $data = mysqli_query($conn, "SELECT * FROM mhs INNER JOIN irs ON irs.nim = mhs.nim WHERE mhs.nama LIKE '%".$Cari."%'");
-                        }else{
-                            $data = mysqli_query($conn, "SELECT * FROM mhs INNER JOIN irs ON irs.nim = mhs.nim");
-                        }
-                        $no = 1;
-                        while ($row = mysqli_fetch_array($data)) {
-                            echo "<tr>";
-                            echo "<th>".$no."</th>";
-                            echo "<td>".$row['nama']."</td>";
-                            echo "<td>".$row['nim']."</td>";
-                            echo "<td>".$row['angkatan']."</td>";
-                            echo "<td>".$row['verif']."</td>";
-                            echo '<td><a class="btn btn-info" href="#">Lihat</a></td>';
-                            echo '<td><a class="btn btn-warning" href="verif_irs.php?nim='.$row['nim'].'">Verif</a></td>';
-                            echo '<td><a class="btn btn-danger" href="cancel_verif_irs.php?nim='.$row["nim"].'">Cancel</a></td>';
-                            echo "</tr>";
-                            $no++;
-                            
-                        }
-                        echo '</tbody>';
-                        echo '</table>';
-    
-                        $data->free();
-                        $conn->close();
-
-                        
-                       
-                        ?>
-                        </tbody>
-                        </table> 
+                                        
+                                    
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
-
-                <!-- Topbar -->
-                <!-- End of Topbar -->
-
-                <!-- Begin Page Content -->
-
-              <!-- General Form Elements -->
                 <!-- /.container-fluid -->
 
             </div>
@@ -143,7 +126,7 @@ $nim = $_SESSION['username'];
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="index.php">Logout</a>
+                    <a class="btn btn-primary" href="login.html">Logout</a>
                 </div>
             </div>
         </div>
@@ -158,6 +141,13 @@ $nim = $_SESSION['username'];
 
     <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/datatables-demo.js"></script>
 
 </body>
 
